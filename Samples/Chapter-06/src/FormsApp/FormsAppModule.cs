dde2ebe19200e78139bec86f76f0b39cea2ff466
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-04-11 21:57:31 +0300

    initial formsapp code

---
+++ Samples/Chapter-06/src/FormsApp/FormsAppModule.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace FormsApp
+{
+
+    [DependsOn(
+        typeof(AbpAutofacModule)
+    )]
+    public class FormsAppModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var configuration = context.Services.GetConfiguration();
+            var hostEnvironment = context.Services.GetSingletonInstance<IHostEnvironment>();
+
+            context.Services.AddHostedService<FormsAppHostedService>();
+        }
+    }
+}
