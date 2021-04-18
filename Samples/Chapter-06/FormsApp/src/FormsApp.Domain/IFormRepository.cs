f5cfc2d023007d3451f61f6bbfc1c095b1b9ddaf
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-04-18 03:40:30 +0300

    Create IFormRepository.cs

---
+++ Samples/Chapter-06/FormsApp/src/FormsApp.Domain/IFormRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
+
+namespace FormsApp
+{
+    public interface IFormRepository : IRepository<Form, Guid>
+    {
+        Task<List<Form>> GetListAsync(
+            string name,
+            bool includeDrafts = false
+        );
+    }
+}
