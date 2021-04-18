a1e02fcdabe97d1b14e8d86f9d8ec97642708679
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-04-18 17:44:47 +0300

    added FormsApp.EntityFrameworkCore.csproj

---
+++ Samples/Chapter-06/FormsApp/src/FormsApp.EntityFrameworkCore/FormsAppDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore.Modeling;
+
+namespace FormsApp
+{
+    public class FormsAppDbContext : AbpDbContext<FormsAppDbContext>
+    {
+        public DbSet<Form> Forms { get; set; }
+
+        public FormsAppDbContext(DbContextOptions<FormsAppDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Form>(b =>
+            {
+                b.ToTable("Forms");
+                b.ConfigureByConvention();
+                b.Property(x => x.Name)
+                    .HasMaxLength(100)
+                    .IsRequired();
+                b.HasIndex(x => x.Name);
+            });
+
+            builder.Entity<Question>(b =>
+            {
+                b.ToTable("FormQuestions");
+                b.ConfigureByConvention();
+                b.Property(x => x.Title)
+                    .HasMaxLength(200)
+                    .IsRequired();
+                b.HasOne<Form>()
+                    .WithMany(x => x.Questions)
+                    .HasForeignKey(x => x.FormId)
+                    .IsRequired();
+            });
+        }
+    }
+}
---
+++ Samples/Chapter-06/FormsApp/src/FormsApp.EntityFrameworkCore/FormsEntityFrameworkCoreModule.cs
@@ -0,0 +1,22 @@
+﻿using FormsApp.Domain;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp.EntityFrameworkCore.SqlServer;
+using Volo.Abp.Modularity;
+
+namespace FormsApp
+{
+[DependsOn(
+    typeof(FormsDomainModule),
+    typeof(AbpEntityFrameworkCoreSqlServerModule)
+    )]
+public class FormsEntityFrameworkCoreModule : AbpModule
+{
+public override void ConfigureServices(ServiceConfigurationContext context)
+{
+    context.Services.AddAbpDbContext<FormsAppDbContext>(options =>
+    {
+        options.AddDefaultRepositories(includeAllEntities: true);
+    });
+}
+}
+}
