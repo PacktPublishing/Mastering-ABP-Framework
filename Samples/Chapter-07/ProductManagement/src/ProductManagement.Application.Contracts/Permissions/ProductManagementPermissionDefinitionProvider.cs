90e6eec233ac57404867f6ae0b2e71d5efb61b7a
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-04-26 02:09:11 +0300

    Added example code parts.

--- Samples/Chapter-07/ProductManagement/src/ProductManagement.Application.Contracts/Permissions/ProductManagementPermissionDefinitionProvider.cs
+++ Samples/Chapter-07/ProductManagement/src/ProductManagement.Application.Contracts/Permissions/ProductManagementPermissionDefinitionProvider.cs
@@ -4,19 +4,27 @@ using Volo.Abp.Localization;
 
 namespace ProductManagement.Permissions
 {
-    public class ProductManagementPermissionDefinitionProvider : PermissionDefinitionProvider
+    public class ProductManagementPermissionDefinitionProvider
+        : PermissionDefinitionProvider
     {
-        public override void Define(IPermissionDefinitionContext context)
+        public override void Define(
+            IPermissionDefinitionContext context)
         {
-            var myGroup = context.AddGroup(ProductManagementPermissions.GroupName);
-
-            //Define your own permissions here. Example:
-            //myGroup.AddPermission(ProductManagementPermissions.MyPermission1, L("Permission:MyPermission1"));
+            var myGroup = context.AddGroup(
+                "ProductManagement",
+                L("ProductManagement"));
+            myGroup.AddPermission(
+                "ProductManagement.ProductCreation",
+                L("ProductCreation"));
+            myGroup.AddPermission(
+                "ProductManagement.ProductDeletion",
+                L("ProductDeletion"));
         }
 
         private static LocalizableString L(string name)
         {
-            return LocalizableString.Create<ProductManagementResource>(name);
+            return LocalizableString
+                .Create<ProductManagementResource>(name);
         }
     }
 }
--- Samples/Chapter-07/ProductManagement/src/ProductManagement.HttpApi/Controllers/ProductController.cs
+++ Samples/Chapter-07/ProductManagement/src/ProductManagement.HttpApi/Controllers/ProductController.cs
@@ -3,9 +3,62 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc;
 
 namespace ProductManagement.Controllers
 {
+    public class ProductController : AbpController
+    {
+        private readonly IAuthorizationService _authorizationService;
+
+        public ProductController(IAuthorizationService authorizationService)
+        {
+            _authorizationService = authorizationService;
+        }
+        
+        public async Task CreateAsync(ProductCreationDto input)
+        {
+            if (await _authorizationService
+                .IsGrantedAsync("ProductManagement.ProductCreation"))
+            {
+                //TODO: Create the product
+            }
+            else
+            {
+                //TODO: Handle unauthorized case
+            }
+        }
+        
+        public async Task CreateAlternativeAsync(ProductCreationDto input)
+        {
+            await _authorizationService
+                .CheckAsync("ProductManagement.ProductCreation");
+            //TODO: Create the product
+        }
+    }
+    
+    /* Using permission based authorization with [Authorize] attribute */
+    /*
+    public class ProductController : Controller
+    {
+        public async Task<List<ProductDto>> GetListAsync()
+        {
+        }
+
+        [Authorize("ProductManagement.ProductCreation")]
+        public async Task CreateAsync(ProductCreationDto input)
+        {
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+        }
+    }
+    */
+
+    /* Shows simple usage of [Authorize] and [AllowAnonymous] attributes on an MVC controller */
+
+    /*
     public class ProductController : Controller
     {
         public async Task<List<ProductDto>> GetListAsync()
@@ -24,7 +77,7 @@ namespace ProductManagement.Controllers
     }
 
     [Authorize]
-    public class AlternativeProductController : Controller
+    public class ProductController : Controller
     {
         [AllowAnonymous]
         public async Task<List<ProductDto>> GetListAsync()
@@ -47,4 +100,6 @@ namespace ProductManagement.Controllers
     public class ProductDto
     {
     }
+    
+    */
 }
