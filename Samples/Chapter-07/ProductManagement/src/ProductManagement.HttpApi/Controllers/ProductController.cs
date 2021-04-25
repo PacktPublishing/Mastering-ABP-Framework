96c75979c36b7c7ebce17de3e433b85840d42c8d
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-04-25 23:45:21 +0300

    Create ProductController.cs

---
+++ Samples/Chapter-07/ProductManagement/src/ProductManagement.HttpApi/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductManagement.Controllers
+{
+    public class ProductController : Controller
+    {
+        public async Task<List<ProductDto>> GetListAsync()
+        {
+        }
+
+        [Authorize]
+        public async Task CreateAsync(ProductCreationDto input)
+        {
+        }
+
+        [Authorize]
+        public async Task DeleteAsync(Guid id)
+        {
+        }
+    }
+
+    [Authorize]
+    public class AlternativeProductController : Controller
+    {
+        [AllowAnonymous]
+        public async Task<List<ProductDto>> GetListAsync()
+        {
+        }
+
+        public async Task CreateAsync(ProductCreationDto input)
+        {
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+        }
+    }
+
+    public class ProductCreationDto
+    {
+    }
+
+    public class ProductDto
+    {
+    }
+}
