c036a63199015a0132f43e9345092296efdb5c91
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-05-15 19:50:00 +0300

    Changed_CategoryName_Index_To_Unique

---
+++ Samples/Chapter-03/ProductManagement/src/ProductManagement.EntityFrameworkCore.DbMigrations/Migrations/20210515145519_Changed_CategoryName_Index_To_Unique.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProductManagement.Migrations
+{
+    public partial class Changed_CategoryName_Index_To_Unique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_Name",
+                table: "Categories");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Name",
+                table: "Categories",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_Name",
+                table: "Categories");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_Name",
+                table: "Categories",
+                column: "Name");
+        }
+    }
+}
