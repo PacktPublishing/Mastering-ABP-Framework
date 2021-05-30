228e7b9247235d24fda568285182d99f4280861e
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-05-30 18:45:21 +0300

    Added examples

---
+++ Samples/Chapter-08/DemoApp/src/DemoApp.Application/MyAppService.cs
@@ -0,0 +1,19 @@
+﻿using DemoApp.Localization;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace DemoApp
+{
+    public class MyAppService : ApplicationService
+    {
+        public MyAppService()
+        {
+            //LocalizationResource = typeof(DemoAppResource); //no need since it is the default resource
+        }
+
+        public async Task FooAsync()
+        {
+            var str = L["WelcomeMessage"];
+        }
+    }
+}
---
+++ Samples/Chapter-08/DemoApp/src/DemoApp.Domain/LocalizationDemoService.cs
@@ -0,0 +1,26 @@
+﻿using DemoApp.Localization;
+using Microsoft.Extensions.Localization;
+using Volo.Abp.DependencyInjection;
+
+namespace DemoApp
+{
+    public class LocalizationDemoService : ITransientDependency
+    {
+        private readonly IStringLocalizer<DemoAppResource> _localizer;
+
+        public LocalizationDemoService(IStringLocalizer<DemoAppResource> localizer)
+        {
+            _localizer = localizer;
+        }
+
+        public string GetWelcomeMessage()
+        {
+            return _localizer["WelcomeMessage"];
+        }
+
+        public string GetWelcomeMessage(string name)
+        {
+            return _localizer["WelcomeMessage", name];
+        }
+    }
+}
