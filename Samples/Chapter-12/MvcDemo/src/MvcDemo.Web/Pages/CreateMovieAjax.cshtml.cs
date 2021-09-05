dde4040ef1ee91111f8f81fb3033a1a6dfeedccf
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-09-05 19:38:50 +0300

    Created AJAX example

---
+++ Samples/Chapter-12/MvcDemo/src/MvcDemo.Web/Pages/CreateMovieAjax.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+
+namespace MvcDemo.Web.Pages
+{
+    public class CreateMovieAjaxModel : AbpPageModel
+    {
+        [BindProperty]
+        public MovieViewModel Movie { get; set; }
+        
+        public void OnGet()
+        {
+            Movie = new MovieViewModel();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            ValidateModel();
+            
+            //TODO: Create a new movie
+            return NoContent();
+        }
+    }
+}
