bafc8da0c41084b41dc7087231459fc639b34f3d
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-09-06 18:29:02 +0300

    added modals demo

---
+++ Samples/Chapter-12/MvcDemo/src/MvcDemo.Web/Pages/ModalsDemo.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvcDemo.Web.Pages
+{
+    public class ModalsDemo : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ Samples/Chapter-12/MvcDemo/src/MvcDemo.Web/Pages/MySimpleModal.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Volo.Abp.AspNetCore.Mvc.UI.RazorPages;
+
+namespace MvcDemo.Web.Pages
+{
+    public class MySimpleModalModel : AbpPageModel
+    {
+        public string Message { get; set; }
+        
+        public void OnGet(int productId)
+        {
+            Message = "Hello modals! " + productId;
+        }
+    }
+}
