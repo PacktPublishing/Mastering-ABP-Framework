efaeefa17ca1bf7a17a976ff67e19bb2b2256f94
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-09-07 11:59:46 +0300

    Added modal script example

---
+++ Samples/Chapter-12/MvcDemo/src/MvcDemo.Web/Pages/ModalWithForm.cshtml.js
@@ -0,0 +1,12 @@
+﻿abp.modals.MovieCreation = function () {
+     this.initModal = function(modalManager, args) {
+        var $modal = modalManager.getModal();
+        var preOrderCheckbox = $modal.find('input[name="Movie.PreOrder"]');
+        preOrderCheckbox.change(function(){
+            if (this.checked){
+               alert('checked pre-order!'); 
+            }
+        });
+        console.log('initialized the modal...');
+    }
+};
