b2a5160f2a49d78913b714c3fd804b3f5e1d3a2f
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-10-11 14:23:23 +0300

    Added examples

---
+++ Samples/Chapter-16/src/MtDemo.Application/MyService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.MultiTenancy;
+
+namespace MtDemo
+{
+    public class MyAppService : ApplicationService
+    {
+        public async Task DoItAsync()
+        {
+            Guid? tenantId = CurrentTenant.Id;
+        }
+    }
+}
---
+++ Samples/Chapter-16/src/MtDemo.Domain/MyService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.MultiTenancy;
+
+namespace MtDemo
+{
+    public class MyService : ITransientDependency
+    {
+        private readonly ICurrentTenant _currentTenant;
+
+        public MyService(ICurrentTenant currentTenant)
+        {
+            _currentTenant = currentTenant;
+        }
+        
+        public async Task DoItAsync()
+        {
+            Guid? tenantId = _currentTenant.Id;
+            string tenantName = _currentTenant.Name;
+        }
+    }
+}
