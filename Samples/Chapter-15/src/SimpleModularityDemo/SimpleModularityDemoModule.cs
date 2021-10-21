9bfa5b0b76540546b2ef04e0dbca73116451c410
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-10-21 11:53:59 +0300

    Added SimpleModularityDemoModule

---
+++ Samples/Chapter-15/src/SimpleModularityDemo/SimpleModularityDemoModule.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Volo.Abp.Autofac;
+using Volo.Abp.Modularity;
+
+namespace SimpleModularityDemo
+{
+
+    [DependsOn(
+        typeof(AbpAutofacModule)
+    )]
+    public class SimpleModularityDemoModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            var configuration = context.Services.GetConfiguration();
+            var hostEnvironment = context.Services.GetSingletonInstance<IHostEnvironment>();
+
+            context.Services.AddHostedService<SimpleModularityDemoHostedService>();
+        }
+    }
+}
