5c972ba54eaf1118e84cab83837e927bde4022e9
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-12-21 11:59:38 +0300

    Entity Framework Core & Database Mappings

--- Samples/Chapter-03/ProductManagement/src/ProductManagement.EntityFrameworkCore/Migrations/20211221085559_Added_Categories_And_Products.Designer.cs
+++ Samples/Chapter-03/ProductManagement/src/ProductManagement.EntityFrameworkCore/Migrations/20211221085559_Added_Categories_And_Products.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProductManagement.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,10 @@ using Volo.Abp.EntityFrameworkCore;
 namespace ProductManagement.Migrations
 {
     [DbContext(typeof(ProductManagementDbContext))]
-    partial class ProductManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211221085559_Added_Categories_And_Products")]
+    partial class Added_Categories_And_Products
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
